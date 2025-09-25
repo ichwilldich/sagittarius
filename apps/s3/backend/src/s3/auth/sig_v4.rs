184f8617e37739ea3f8219f0d7ce1bd8c57a74d1
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-25 22:38:28 +0200

    test: added s3 signature auth tests

--- apps/s3/backend/src/s3/auth/sig_v4.rs
+++ apps/s3/backend/src/s3/auth/sig_v4.rs
@@ -208,3 +208,219 @@ fn hmac(key: &[u8], msg: &str) -> Result<Vec<u8>> {
   hmac.update(msg.as_bytes());
   Ok(hmac.finalize().into_bytes().to_vec())
 }
+
+#[cfg(test)]
+mod test {
+  use axum::extract::Request;
+
+  use super::*;
+
+  #[test]
+  fn empty_string_sha256() {
+    let hash = hex::encode(Sha256::digest("".as_bytes()));
+    assert_eq!(hash, EMPTY_STRING_SHA256_HASH);
+  }
+
+  #[test]
+  fn algorithm() {
+    // just so ai doesn't mess with it
+    assert_eq!(ALGORITHM, "AWS4-HMAC-SHA256");
+    assert_eq!(ALGORITHM_CHUNKED, "AWS4-HMAC-SHA256-PAYLOAD");
+  }
+
+  fn aws4() -> AWS4 {
+    AWS4 {
+      credential: AWS4Credential {
+        access_key: "AKIAIOSFODNN7EXAMPLE".to_string(),
+        date: "20130524".to_string(),
+        region: "us-east-1".to_string(),
+      },
+      signature: String::new(),
+      signed_headers: vec![
+        "host".to_string(),
+        "x-amz-content-sha256".to_string(),
+        "x-amz-date".to_string(),
+      ],
+    }
+  }
+
+  fn parts() -> Parts {
+    let req = Request::new(());
+    let mut parts = req.into_parts().0;
+
+    parts.method = http::Method::GET;
+    parts.uri = "/test.txt?a=b&c=d&b=w".parse().unwrap();
+    parts
+      .headers
+      .insert("Host", "examplebucket.s3.amazonaws.com".parse().unwrap());
+    parts
+      .headers
+      .insert("x-amz-date", "20130524T000000Z".parse().unwrap());
+    parts
+      .headers
+      .insert("x-amz-content-sha256", "UNSIGNED-PAYLOAD".parse().unwrap());
+
+    parts
+  }
+
+  #[test]
+  fn cr_unsigned() {
+    let parts = parts();
+    let mut auth = aws4();
+    let payload = Payload::Unsigned;
+
+    let canonical_request = CanonicalRequest::new(&parts, &mut auth, &payload);
+    let expected_canonical_request = "GET\n\
+    /test.txt\n\
+    a=b&b=w&c=d\n\
+    host:examplebucket.s3.amazonaws.com\n\
+    x-amz-content-sha256:UNSIGNED-PAYLOAD\n\
+    x-amz-date:20130524T000000Z\n\
+    \n\
+    host;x-amz-content-sha256;x-amz-date\n\
+    UNSIGNED-PAYLOAD";
+
+    assert_eq!(canonical_request.0, expected_canonical_request);
+  }
+
+  #[test]
+  fn cr_single_chunk() {
+    let parts = parts();
+    let mut auth = aws4();
+    let payload = Payload::SingleChunk(EMPTY_STRING_SHA256_HASH.to_string());
+
+    let canonical_request = CanonicalRequest::new(&parts, &mut auth, &payload);
+    let expected_canonical_request = "GET\n\
+    /test.txt\n\
+    a=b&b=w&c=d\n\
+    host:examplebucket.s3.amazonaws.com\n\
+    x-amz-content-sha256:UNSIGNED-PAYLOAD\n\
+    x-amz-date:20130524T000000Z\n\
+    \n\
+    host;x-amz-content-sha256;x-amz-date\n\
+    e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+    assert_eq!(canonical_request.0, expected_canonical_request);
+  }
+
+  #[test]
+  fn cr_multiple_chunks() {
+    let parts = parts();
+    let mut auth = aws4();
+    let payload = Payload::MultipleChunks;
+
+    let canonical_request = CanonicalRequest::new(&parts, &mut auth, &payload);
+    let expected_canonical_request = "GET\n\
+    /test.txt\n\
+    a=b&b=w&c=d\n\
+    host:examplebucket.s3.amazonaws.com\n\
+    x-amz-content-sha256:UNSIGNED-PAYLOAD\n\
+    x-amz-date:20130524T000000Z\n\
+    \n\
+    host;x-amz-content-sha256;x-amz-date\n\
+    STREAMING-AWS4-HMAC-SHA256-PAYLOAD";
+
+    assert_eq!(canonical_request.0, expected_canonical_request);
+  }
+
+  fn cr() -> CanonicalRequest {
+    let parts = parts();
+    let mut auth = aws4();
+    let payload = Payload::Unsigned;
+
+    CanonicalRequest::new(&parts, &mut auth, &payload)
+  }
+
+  #[test]
+  fn cr_sts() {
+    let canonical_request = cr();
+    let amz_date = DateTime::parse_from_rfc3339("2013-05-24T00:00:00Z")
+      .unwrap()
+      .with_timezone(&Utc);
+    let credential = &aws4().credential;
+
+    let string_to_sign = canonical_request.string_to_sign(&amz_date, credential);
+    let expected_string_to_sign = "AWS4-HMAC-SHA256\n\
+    20130524T000000Z\n\
+    20130524/us-east-1/s3/aws4_request\n\
+    a8ed10a1bc6059e6b958a64277969dcdc70444f16bed458169f04592b2fd4d98";
+
+    assert_eq!(string_to_sign.0, expected_string_to_sign);
+  }
+
+  #[test]
+  fn sts_chunked() {
+    let amz_date = DateTime::parse_from_rfc3339("2013-05-24T00:00:00Z")
+      .unwrap()
+      .with_timezone(&Utc);
+    let credential = &aws4().credential;
+    let previous_signature = "a8ed10a1bc6059e6b958a64277969dcdc70444f16bed458169f04592b2fd4d98";
+    let current_data = b"Hello World";
+
+    let string_to_sign =
+      StringToSign::chunked(&amz_date, credential, previous_signature, current_data);
+    let expected_string_to_sign = "AWS4-HMAC-SHA256-PAYLOAD\n\
+    20130524T000000Z\n\
+    20130524/us-east-1/s3/aws4_request\n\
+    a8ed10a1bc6059e6b958a64277969dcdc70444f16bed458169f04592b2fd4d98\n\
+    e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\n\
+    a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e";
+
+    assert_eq!(string_to_sign.0, expected_string_to_sign);
+  }
+
+  #[test]
+  fn sts_chunked_trailer() {
+    let amz_date = DateTime::parse_from_rfc3339("2013-05-24T00:00:00Z")
+      .unwrap()
+      .with_timezone(&Utc);
+    let credential = &aws4().credential;
+    let previous_signature = "a8ed10a1bc6059e6b958a64277969dcdc70444f16bed458169f04592b2fd4d98";
+    let trailing_header_name = "x-amz-checksum-sha256";
+    let trailing_header_value = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e";
+    let string_to_sign = StringToSign::chunked_trailer(
+      &amz_date,
+      credential,
+      previous_signature,
+      trailing_header_name,
+      trailing_header_value,
+    );
+    let expected_string_to_sign = "AWS4-HMAC-SHA256-PAYLOAD\n\
+    20130524T000000Z\n\
+    20130524/us-east-1/s3/aws4_request\n\
+    a8ed10a1bc6059e6b958a64277969dcdc70444f16bed458169f04592b2fd4d98\n\
+    x-amz-checksum-sha256:a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e\n";
+    assert_eq!(string_to_sign.0, expected_string_to_sign);
+  }
+
+  fn sts() -> StringToSign {
+    let canonical_request = cr();
+    let amz_date = DateTime::parse_from_rfc3339("2013-05-24T00:00:00Z")
+      .unwrap()
+      .with_timezone(&Utc);
+    let credential = &aws4().credential;
+
+    canonical_request.string_to_sign(&amz_date, credential)
+  }
+
+  #[test]
+  fn sign() {
+    let secret_key = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY";
+    let credential = &aws4().credential;
+    let string_to_sign = sts().sign(secret_key, credential);
+    let expected_signature = "e8c68eaa3147a30f4cde5eca6a0888571e3716ff1856c5cebc916ddcb6a7eb0a";
+    assert_eq!(string_to_sign.unwrap(), expected_signature);
+  }
+
+  #[test]
+  fn hmac() {
+    let key = b"wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY";
+    let msg = "20130524/us-east-1/s3/aws4_request";
+    let hmac = super::hmac(key, msg).unwrap();
+    let expected_hmac = vec![
+      16, 222, 136, 98, 3, 7, 133, 154, 17, 212, 111, 136, 227, 180, 36, 141, 59, 240, 137, 150,
+      27, 24, 204, 16, 123, 95, 4, 28, 218, 126, 221, 188,
+    ];
+    assert_eq!(hmac, expected_hmac);
+  }
+}
