3516b3c3efec28f34217fbc7ce77ad760508b0c0
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-09-26 16:15:59 +0200

    test: added s3 multipart auth tests

--- apps/s3/backend/src/s3/auth/multipart.rs
+++ apps/s3/backend/src/s3/auth/multipart.rs
@@ -102,3 +102,89 @@ async fn parse_multipart(
 
   Ok(multipart_data)
 }
+
+#[cfg(test)]
+mod test {
+  use http::header::CONTENT_TYPE;
+  use mime::BOUNDARY;
+  use std::io::Write;
+
+  use super::*;
+
+  fn write_field(multipart: &mut Vec<u8>, key: &str, content: &str) {
+    write!(multipart, "--{}\r\n", BOUNDARY).unwrap();
+    write!(
+      multipart,
+      "Content-Disposition: form-data; name=\"{}\"\r\n\r\n",
+      key
+    )
+    .unwrap();
+    write!(multipart, "{}\r\n", content).unwrap();
+  }
+
+  fn multipart() -> Result<Request> {
+    let mut multipart: Vec<u8> = Vec::new();
+
+    let policy = r#"{}"#;
+    let policy_b64 = BASE64_STANDARD.encode(policy);
+
+    // meta
+    write_field(&mut multipart, "key", "test.txt");
+    write_field(&mut multipart, "policy", &policy_b64);
+    write_field(&mut multipart, "x-amz-algorithm", ALGORITHM);
+    write_field(
+      &mut multipart,
+      "x-amz-credential",
+      "test/21240426/us-east-1/s3/aws4_request",
+    );
+    write_field(&mut multipart, "x-amz-date", "20240426T000000Z");
+    write_field(
+      &mut multipart,
+      "x-amz-signature",
+      "7a03f608465682aeb5bd284b7c2bfa84dd90bc5e4dc118cad4b22ec1754dfcb2",
+    );
+
+    // file
+    write!(multipart, "--{}\r\n", BOUNDARY).unwrap();
+    write!(
+      multipart,
+      "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n"
+    )
+    .unwrap();
+    write!(multipart, "Content-Type: text/plain\r\n\r\n").unwrap();
+    write!(multipart, "Hello, world!\r\n").unwrap();
+    write!(multipart, "--{}--\r\n", BOUNDARY).unwrap();
+
+    Ok(
+      Request::builder()
+        .header(
+          CONTENT_TYPE,
+          format!("multipart/form-data; boundary={}", BOUNDARY),
+        )
+        .body(multipart.into())
+        .unwrap(),
+    )
+  }
+
+  #[tokio::test]
+  async fn test_multipart_auth() -> Result<()> {
+    let req = multipart()?;
+    let auth: S3Auth<Vec<u8>> = multipart_auth(req).await?;
+
+    assert_eq!(auth.identity, Identity::AccessKey("test".to_string()));
+    assert_eq!(auth.body, b"Hello, world!".to_vec());
+
+    Ok(())
+  }
+
+  #[tokio::test]
+  async fn test_parse_multipart() -> Result<()> {
+    let req = multipart()?;
+    let multipart = Multipart::from_request(req, &()).await?;
+
+    let result = parse_multipart(multipart, &mut <() as BodyWriter>::new().await?).await;
+    assert!(result.is_ok());
+
+    Ok(())
+  }
+}
