cb5a887c4d29887b206d7b5056e584f38f74eab0
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-01 09:45:33 +0200

    fix: build.rs

--- backend/build.rs
+++ backend/build.rs
@@ -6,8 +6,8 @@ fn main() {
     println!("cargo:rustc-env=FRONTEND_DIR={}", frontend_dir);
 
     let frontend_port =
-      std::env::var("FRONTEND_PORT").unwrap_or_else(|_| "http://localhost:3000".to_string());
-    println!("cargo:rerun-if-env-changed=FRONTEND_PORT");
-    println!("cargo:rustc-env=FRONTEND_PORT={}", frontend_port);
+      std::env::var("FRONTEND_URL").unwrap_or_else(|_| "http://localhost:3000".to_string());
+    println!("cargo:rerun-if-env-changed=FRONTEND_URL");
+    println!("cargo:rustc-env=FRONTEND_URL={}", frontend_port);
   }
 }
