3a32ed86f81c53f095e7e344cb1d0e3ad6443ce4
CodeMaster4711 <cedi27@t-online.de>
2025-10-05 13:43:34 +0200

    feat: added s3 xml resp syntax

--- backend/src/s3/operations/bucket.rs
+++ backend/src/s3/operations/bucket.rs
@@ -1,10 +1,10 @@
 use axum::{
-  Json, Router,
+  Router,
   extract::Path,
   routing::{delete, get, put},
 };
 use centaurus::{error::Result, req::xml::Xml};
-use http::HeaderMap;
+use http::{HeaderMap, header::CONTENT_TYPE};
 use serde::Deserialize;
 
 use crate::s3::{
@@ -64,9 +64,43 @@ async fn delete_bucket(
   Ok(())
 }
 
-async fn list_buckets(interface: S3Interface) -> Result<Json<Vec<String>>> {
+async fn list_buckets(interface: S3Interface) -> Result<(HeaderMap, String)> {
   let buckets = interface.list_buckets().await?;
-  Ok(Json(buckets))
+
+  // einfache XML-Escaping-Funktion
+  fn xml_escape(s: &str) -> String {
+    s.chars()
+      .map(|c| match c {
+        '&' => "&amp;".into(),
+        '<' => "&lt;".into(),
+        '>' => "&gt;".into(),
+        '"' => "&quot;".into(),
+        '\'' => "&apos;".into(),
+        other => other.to_string(),
+      })
+      .collect()
+  }
+
+  let mut xml = String::new();
+  xml.push_str(r#"<?xml version="1.0" encoding="UTF-8"?>"#);
+  xml.push_str(r#"<ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">"#);
+  xml.push_str("<Buckets>");
+  for name in buckets {
+    xml.push_str("<Bucket>");
+    // nur Name + CreationDate minimal (CreationDate leer, kann ggf. mit echtem Timestamp gefüllt werden)
+    xml.push_str(&format!("<Name>{}</Name>", xml_escape(&name)));
+    xml.push_str("<CreationDate></CreationDate>");
+    xml.push_str("</Bucket>");
+  }
+  xml.push_str("</Buckets>");
+  // einfacher Owner-Block; anpassen falls echte Owner-Infos vorhanden sind
+  xml.push_str("<Owner><DisplayName>owner</DisplayName><ID>ownerid</ID></Owner>");
+  xml.push_str("</ListAllMyBucketsResult>");
+
+  let mut headers = HeaderMap::new();
+  headers.insert(CONTENT_TYPE, "application/xml".parse().unwrap());
+
+  Ok((headers, xml))
 }
 
 /// TODO: Handling of additional configuration options
